50d131f0d5c1d41b75f12310f6d27528fbdd1e3d
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2014-10-11 18:14:26 +0900

    tmux カレントパスの設定 add, bash cdの効率化 add

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/dotfiles/_bashrc ~/.bashrc
 ln -s ~/dotfiles/_bash_profile ~/.bash_profile
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/_cd_conf ~/.cd_conf
